580bb6eaba7f05d9d1509706b7be6af13bd922f1
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-23 19:55:04 +0100

    Split long docker command into multiple lines and add quick test script

---
+++ curls.sh
@@ -0,0 +1,5 @@
+curl localhost:8282/
+curl localhost:8282/
+curl localhost:8282/hellolua
+curl localhost:8282/hellolua
+docker logs proxy-state
--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-docker run --rm --detach --volume `pwd`/conf.d:/etc/nginx/conf.d --publish 8282:80 --name proxy-state proxy-state
+docker run \
+    --rm \
+    --detach \
+    --volume `pwd`/conf.d:/etc/nginx/conf.d \
+    --publish 8282:80 \
+    --name proxy-state \
+    proxy-state
