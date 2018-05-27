ea86f002d9a0d8c5cee6cd7623af6d0ee76e1d56
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-27 04:26:31 +0100

    Remove duplication and add support for returning stats in JSON format

--- lualib/Proxy.lua
+++ lualib/Proxy.lua
@@ -1,13 +1,34 @@
 local template = require "resty.template"
+local cjson = require "cjson"
 
 local M = {}
 
 local stats = {}
 
--- TODO: order HTTP status ranges
--- TODO: limit avg_latency_ms to 2 decimal places
--- TODO: refactor to remove duplication
--- TODO: make stats available in JSON format (/proxy/stats?format=json)
+function createTargetStats(latency, status)
+    local targetStats = {}
+    targetStats.count = 1
+    targetStats.avg_latency_ms = latency
+    targetStats.http_status = {};
+    targetStats.http_status["2XX"] = 0
+    targetStats.http_status["3XX"] = 0
+    targetStats.http_status["4XX"] = 0
+    targetStats.http_status["5XX"] = 0
+    incrementHttpStatusCount(targetStats.http_status, status)
+    return targetStats
+end
+
+function incrementHttpStatusCount(http_status, status)
+    if status >= 200 and status < 300 then
+        http_status["2XX"] = http_status["2XX"] + 1
+    elseif status >= 300 and status < 400 then
+        http_status["3XX"] = http_status["3XX"] + 1
+    elseif status >= 400 and status < 500 then
+        http_status["4XX"] = http_status["4XX"] + 1
+    else
+        http_status["5XX"] = http_status["5XX"] + 1
+    end
+end
 
 function M.addTargetStats()
     local target = ngx.var.arg_target
@@ -16,46 +37,27 @@ function M.addTargetStats()
         local latency = 1000 * (tonumber(ngx.var.upstream_response_time) or 0)
         local targetStats = stats[target]
         if targetStats then
-            targetStats.count = targetStats.count + 1
-            targetStats.total_latency_ms = targetStats.total_latency_ms + latency
-            targetStats.avg_latency_ms = targetStats.total_latency_ms / targetStats.count
-            local http_status = targetStats.http_status
-            if status >= 200 and status < 300 then
-                http_status["2XX"] = http_status["2XX"] + 1
-            elseif status >= 300 and status < 400 then
-                http_status["3XX"] = http_status["3XX"] + 1
-            elseif status >= 400 and status < 500 then
-                http_status["4XX"] = http_status["4XX"] + 1
-            else
-                http_status["5XX"] = http_status["5XX"] + 1
-            end
+            local oldCount = targetStats.count
+            local newCount = oldCount + 1
+            targetStats.count = newCount
+            local avg = (targetStats.avg_latency_ms * oldCount + latency) / newCount
+            local avgToTwoDecimalPlaces = tonumber(string.format("%.2f", avg))
+            targetStats.avg_latency_ms = avgToTwoDecimalPlaces
+            incrementHttpStatusCount(targetStats.http_status, status)
         else
-            stats[target] = {}
-            local targetStats = stats[target]
-            targetStats.count = 1
-            targetStats.total_latency_ms = latency
-            targetStats.avg_latency_ms = latency
-            targetStats.http_status = {};
-            local http_status = targetStats.http_status
-            http_status["2XX"] = 0
-            http_status["3XX"] = 0
-            http_status["4XX"] = 0
-            http_status["5XX"] = 0
-            if status >= 200 and status < 300 then
-                http_status["2XX"] = 1
-            elseif status >= 300 and status < 400 then
-                http_status["3XX"] = 1
-            elseif status >= 400 and status < 500 then
-                http_status["4XX"] = 1
-            else
-                http_status["5XX"] = 1
-            end
+            stats[target] = createTargetStats(latency, status)
         end
     end
 end
 
+-- TODO: how to return correctly ordered http_status tables ?
 function M.getAllStats()
-    template.render("view.html", { stats = stats })
+    if ngx.var.arg_format == "json" then
+        ngx.header.content_type = "application/json";
+        ngx.say(cjson.encode(stats))
+    else
+        template.render("view.html", { stats = stats })
+    end
 end
 
 return M
